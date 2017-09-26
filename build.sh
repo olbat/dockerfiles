e33bf650639b39d4f37ab04493ad1fea33b5fdac
Luc Sarzyniec <devel@olbat.net>
2017-09-26 07:55:39 +0200

    Use Dockerfile ARG instruction to specify the base user and maintainer

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 set -e
 set -x
 
+. build.env
+buildargs="--build-arg BASE_USER=$BASE_USER"
+buildargs+=" --build-arg MAINTAINER=$MAINTAINER"
+
 dir=${IMAGE%/}
 for dockerfile in ${dir:-*}/Dockerfile*
 do
@@ -13,6 +17,6 @@ do
 	tag=${filename##*.}
 	[ "$filename" == "$tag" ] && tag=latest # no file extension
 
-	docker build -f $dockerfile -t $user/$image:$tag $image/
+	docker build $buildargs -f $dockerfile -t $user/$image:$tag $image/
 	[ $PUSH ] && docker push $user/$image:$tag
 done

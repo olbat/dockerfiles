d45d3bfbedfac12c18af8f846eab47f1c94b0722
Luc Sarzyniec <devel@olbat.net>
2017-09-29 17:31:25 +0200

    Add a generic script to generate light dockerfiles

---
+++ gen-light-dockerfile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+[ $# -ge 1 ] && . $1
+
+BASEIMAGE=${BASEIMAGE:-olbat/debian:stable}
+EXECUTABLES=${EXECUTABLES:-"/bin/bash /bin/ls"}
+FILES=${FILES:-"/etc/debian_version"}
+
+cat <<EOF
+FROM $BASEIMAGE AS base
+
+FROM scratch
+ARG MAINTAINER
+MAINTAINER \$MAINTAINER
+
+$(echo -n $EXECUTABLES | xargs -d' ' -n1 -I{} echo "COPY --from=base {} {}")
+
+$(docker run --rm $BASEIMAGE ldd $EXECUTABLES \
+	| grep -v ':\|linux-vdso.so' | cut -d\> -f2 | cut -d\( -f1 \
+	| sort -u | xargs -n1 -I{} echo "COPY --from=base {} {}")
+
+$(echo -n $FILES | xargs -d' ' -n1 -I{} echo "COPY --from=base {} {}")
+
+CMD ["$(echo $EXECUTABLES | cut -d' ' -f1)"]
+EOF

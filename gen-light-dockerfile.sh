ebead939dcf8d79c9f6b33764b5b6fd30728c38d
Luc Sarzyniec <devel@olbat.net>
2017-10-04 08:04:15 +0200

    Allow to specify shared libraries when generating light images

--- gen-light-dockerfile.sh
+++ gen-light-dockerfile.sh
@@ -7,6 +7,7 @@ set -e
 BASEIMAGE=${BASEIMAGE:-olbat/debian:stable}
 EXECUTABLES=${EXECUTABLES:-"/bin/bash /bin/ls"}
 FILES=${FILES:-"/etc/debian_version"}
+SHARED_LIBRARIES=${SHARED_LIBRARIES:-}
 
 cat <<EOF
 FROM $BASEIMAGE AS base
@@ -17,11 +18,11 @@ MAINTAINER \$MAINTAINER
 
 $(echo -n $EXECUTABLES | xargs -d' ' -n1 -I{} echo "COPY --from=base {} {}")
 
-$(docker run --rm $BASEIMAGE ldd $EXECUTABLES \
+$(echo -n $FILES | xargs -d' ' -n1 -I{} echo "COPY --from=base {} {}")
+
+$(docker run --rm $BASEIMAGE sh -c "ldd $EXECUTABLES $SHARED_LIBRARIES" \
 	| grep -v ':\|linux-vdso.so' | cut -d\> -f2 | cut -d\( -f1 \
 	| sort -u | xargs -n1 -I{} echo "COPY --from=base {} {}")
-
-$(echo -n $FILES | xargs -d' ' -n1 -I{} echo "COPY --from=base {} {}")
 
 CMD ["$(echo $EXECUTABLES | cut -d' ' -f1)"]
 EOF

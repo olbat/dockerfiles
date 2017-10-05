ecba99e23108c7bd2a9faac9de9342e5ae725943
Luc Sarzyniec <devel@olbat.net>
2017-10-05 07:56:12 +0200

    Factorize the code to build using different tags of the same base image

--- build.sh
+++ build.sh
@@ -6,17 +6,31 @@ set -x
 . build.env
 buildargs="--build-arg BASE_USER=$BASE_USER"
 buildargs+=" --build-arg MAINTAINER=$MAINTAINER"
-
+user=${BUILD_USER:-olbat}
 dir=${IMAGE%/}
+
 for dockerfile in ${dir:-*}/Dockerfile*
 do
 	[ -e $dockerfile ] || (echo "ERROR: file not found $dockerfile"; exit 1)
-	user=${BUILD_USER:-olbat}
-	image=$(dirname "$dockerfile")
+	dirname=$(dirname "$dockerfile")
 	filename=$(basename "$dockerfile")
-	tag=${filename##*.}
-	[ "$filename" == "$tag" ] && tag=latest # no file extension
+	args="-f $dockerfile $buildargs"
 
-	docker build $buildargs -f $dockerfile -t $user/$image:$tag $image/
-	[ $PUSH ] && docker push $user/$image:$tag
+	if [ -f $dirname/tags.env -a $filename == "Dockerfile" ]
+	then
+		BASE_TAGS=
+		. $dirname/tags.env
+		for tag in $BASE_TAGS
+		do
+			args="$args --build-arg BASE_TAG=$tag"
+			docker build $args -t $user/$dirname:$tag $dirname/
+			[ $PUSH ] && docker push $user/$dirname:$tag
+		done
+	else
+		tag=${filename##*.}
+		[ "$filename" == "$tag" ] && tag=latest # no file extension
+
+		docker build $args -t $user/$dirname:$tag $dirname/
+		[ $PUSH ] && docker push $user/$dirname:$tag
+	fi
 done

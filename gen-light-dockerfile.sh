5254543862528a1640a896f11850383c6f5f50ec
Luc Sarzyniec <devel@olbat.net>
2025-09-15 21:25:45 +0200

    Fix: add support for symlinks in light Dockerfiles on Kaniko
    Kaniko doesn't handle symlinks in the same way than the standard docker
    building tool. When copying a file, it doesn't necessarilly follow them,
    which makes the build of some 'light' images fail on the CI.
    
    This change introduces a new stage in 'light' images, where files are
    copied in a destination directory using the 'cp -L' command which
    correctly follows symlinks. The final stage then simply copies the
    generated root directory into the image.

--- gen-light-dockerfile.sh
+++ gen-light-dockerfile.sh
@@ -9,37 +9,45 @@ EXECUTABLES=${EXECUTABLES:-"/bin/bash /bin/cat"}
 FILES=${FILES:-"/etc/debian_version"}
 SHARED_LIBRARIES=${SHARED_LIBRARIES:-}
 EXECUTABLES_DESTINATION=${EXECUTABLES_DESTINATION:-/bin/}
+LIBRARIES_DESTINATION=${LIBRARIES_DESTINATION:-/lib/}
+ROOT_DIR=${ROOT_DIR:-/tmp/rootdir}
 
 
 cat <<EOF
 FROM $BASEIMAGE AS base
 
-FROM scratch
-ARG MAINTAINER
-MAINTAINER \$MAINTAINER
+RUN mkdir -p $ROOT_DIR$EXECUTABLES_DESTINATION
+RUN cp -L \\
+$(echo -n $EXECUTABLES | sed -e 's:/\(.*\)\[:/\1[[]:g' | xargs -d' ' -n1 -I{} echo -e "\t{} \\")
+	$ROOT_DIR$EXECUTABLES_DESTINATION
 
-COPY --from=base \\
-$(echo -n $EXECUTABLES | xargs -d' ' -n1 -I{} echo -e "\t{} \\")
-	$EXECUTABLES_DESTINATION
+RUN mkdir -p $ROOT_DIR$LIBRARIES_DESTINATION
+$([ $SHARED_LIBRARIES ] && {
+	echo -e "RUN cp -L \\" \
+	&& echo -n $SHARED_LIBRARIES | xargs -d' ' -n1 -I{} echo -e "\t{} \\" \
+	&& echo -e "\t$ROOT_DIR$LIBRARIES_DESTINATION"; })
+RUN cp -L \\
+$(docker run --rm $BASEIMAGE sh -c "ldd $EXECUTABLES $SHARED_LIBRARIES" \
+	| grep -v '^[[:space:]]\+/\|:\|linux-vdso.so\|not a dynamic executable' \
+	| cut -d\> -f2 | cut -d\( -f1 | sort -u \
+	| xargs -n1 -I{} echo -e "\t{} \\")
+	$ROOT_DIR$LIBRARIES_DESTINATION
 
 $(docker run --rm $BASEIMAGE sh -c "ldd $EXECUTABLES $SHARED_LIBRARIES" \
 	| grep '^[[:space:]]\+/' \
 	| cut -d\> -f2 | cut -d\( -f1 | sort -u \
-	| xargs -n1 -I{} echo "COPY --from=base {} {}")
+	| xargs -n1 -I{} echo "RUN install -D {} $ROOT_DIR{}")
 
-$([ $SHARED_LIBRARIES ] && {
-	echo -e "COPY --from=base \\" \
-	&& echo -n $SHARED_LIBRARIES | xargs -d' ' -n1 -I{} echo -e "\t{} \\" \
-	&& echo -e "\t/lib/"; })
+$(echo -n $FILES \
+	| xargs -d' ' -n1 -I{} echo "RUN mkdir -p "'`dirname '"$ROOT_DIR{}"'`'" \\
+	&& cp -rL {} $ROOT_DIR{}")
 
-COPY --from=base \\
-$(docker run --rm $BASEIMAGE sh -c "ldd $EXECUTABLES $SHARED_LIBRARIES" \
-	| grep -v '^[[:space:]]\+/\|:\|linux-vdso.so\|not a dynamic executable'\
-	| cut -d\> -f2 | cut -d\( -f1 | sort -u \
-	| xargs -n1 -I{} echo -e "\t{} \\")
-	/lib/
 
-$(echo -n $FILES | xargs -d' ' -n1 -I{} echo "COPY --from=base {} {}")
+FROM scratch
+ARG MAINTAINER
+MAINTAINER \$MAINTAINER
 
-CMD ["$(echo $EXECUTABLES | cut -d' ' -f1)"]
+COPY --from=0 $ROOT_DIR/ /
+
+CMD ["$(echo $EXECUTABLES | cut -d' ' -f1 | xargs basename | xargs -I{} echo $EXECUTABLES_DESTINATION{})"]
 EOF
